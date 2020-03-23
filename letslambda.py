239ddf66dd7070e16b9b02153ded5f78f0bfa097
Ray Bachert <Ray.Bachert001@msd.govt.nz>
2020-03-23 14:16:02 +1300

    first attempt

--- letslambda.py
+++ letslambda.py
@@ -5,19 +5,11 @@ import boto3
 import hashlib
 import logging
 import os
-import requests
 import yaml
-from acme import challenges
-from acme import client
-from acme import errors
-from acme import messages
-from acme.jose.util import ComparableX509
+import sewer
 from botocore.config import Config
 from botocore.exceptions import ClientError
-from Crypto import Random
-from Crypto.PublicKey import RSA
 from datetime import datetime
-from OpenSSL import crypto
 from time import sleep
 
 LOG = logging.getLogger("letslambda")
@@ -59,55 +51,6 @@ def load_config(s3, s3_bucket, letslambda_config):
         return None
 
     return yaml.load(conf)
-
-def load_letsencrypt_account_key(conf):
-    """
-    Try to load the RSA account key from S3. If it doesn't
-    succeed, it will create a new account key and try a registration
-    with your provided information
-    The letsenrypt account key is needed to avoid redoing the Proof of
-    Possession challenge (PoP). It is also used to revoke an existing
-    certificate.
-    """
-    LOG.debug("Loading account key from s3")
-
-    newAccountNeeded = False
-    account_key = load_from_s3(conf, 'account.key.rsa')
-    if account_key == None:
-        account_key = create_and_save_key(conf, "account.key.rsa", conf['kms_key'])
-        newAccountNeeded = True
-
-    key = client.jose.JWKRSA.load(account_key)
-    if newAccountNeeded:
-        register_new_account(conf, key)
-
-    return key
-
-def register_new_account(conf, key):
-    """
-    Attempt to create a new account on the ACME server
-    with the key. No problem if it fails because this
-    kye is already used.
-    """
-    LOG.info("Registering with ACME server with the new account key")
-    newReg = messages.NewRegistration(contact=tuple(conf['info']), key=key.public_key())
-    acme_client = client.Client(conf['directory'], key)
-    registration_resource = acme_client.register(newReg)
-    LOG.info("Agreeing on the TOS on your behalf")
-    acme_client.agree_to_tos(registration_resource)
-
-def get_authorization(client, domain):
-    authorization_resource = client.request_domain_challenges(domain['name'])
-    return authorization_resource
-
-def get_dns_challenge(authorization_resource):
-    """
-    Ask the ACME server to give us a list of challenges.
-    Later, we will pick only the DNS one.
-    """
-    # Now let's look for a DNS challenge
-    dns_challenges = filter(lambda x: isinstance(x.chall, challenges.DNS01), authorization_resource.body.challenges)
-    return list(dns_challenges)[0]
 
 def get_route53_zone_id(conf, zone_name):
     r53 = boto3.client('route53', config=Config(signature_version='v4', region_name=conf['region']))
@@ -252,153 +195,15 @@ def wait_letsencrypt_record_insync(conf, r53_status):
     LOG.debug("Route53 synchronized in {0:d} seconds.".format(60-timeout))
     return True
 
-def save_certificates_to_s3(conf, domain, chain_certificate, certificate):
+def save_certificates_to_s3(conf, domain, certificate, certificate_key, account_key):
     """
-    Save/overwite newly requested certificate and corresponding chain certificate
+    Save/overwite newly requested certificate, key and account key
     """
-    if chain_certificate is not False:
+    if certificate is not False:
         LOG.info("Saving certificate to S3")
-        save_to_s3(conf, domain['name']+".chain.pem", chain_certificate)
-
-    LOG.info("Saving chain certificate to S3")
-    save_to_s3(conf, domain['name']+".cert.pem", certificate)
-
-
-def upload_to_iam(conf, domain, chain_certificate, certificate, key):
-    """
-    Create a new IAM certificate from ACME and private key.
-    It also fetched the chain certificate from ACME if provided
-    """
-    LOG.info("Loading certificate elements for domain '{0}' into IAM".format(domain['name']))
-
-    iam = boto3.client('iam', config=Config(signature_version='v4', region_name=conf['region']))
-
-    kwargs = {
-        'Path': '/',
-        'ServerCertificateName': domain['name'] + "-" + datetime.utcnow().strftime("%Y-%m-%dT%H-%M-%S"),
-        'CertificateBody': certificate,
-        'PrivateKey': key
-        }
-    try:
-        if chain_certificate is not False:
-            kwargs['CertificateChain'] = chain_certificate
-
-        res = iam.upload_server_certificate(**kwargs)
-    except ClientError as e:
-        LOG.error("Failed to upload certificate for domain '{0}'".format(domain['name']))
-        LOG.error("Exception: {0}".format(e))
-        return False
-
-    return res
-
-def update_elb_server_certificate(conf, domain, server_certificate_arn):
-    """
-    Assign the new SSL certificate to the desired ELB
-    """
-    elb = boto3.client('elb', config=Config(signature_version='v4', region_name=domain['elb_region']))
-
-    timeout = 60
-    while timeout > -1:
-        try:
-            res = elb.set_load_balancer_listener_ssl_certificate(
-                LoadBalancerName=domain['elb'],
-                LoadBalancerPort=domain['elb_port'],
-                SSLCertificateId=server_certificate_arn)
-            break
-        except ClientError as e:
-            if e.response['Error']['Code']  == 'CertificateNotFound':
-                # occasionally server certificate may be reported as not found, even in the same region.
-                # let's give a chance to iam to be aware of our changes especially when  an ELB is in a
-                # different region
-                sleep(1)
-                timeout = timeout - 1
-                continue
-
-            LOG.error("Failed to set server certificate '{0}' on ELB '{0}:{1}' in region '{2}'".format(server_certificate_arn, domain['elb'], domain['elb_port'], domain['elb_region']))
-            LOG.error("Exception: {0}".format(e))
-            return False
-
-    if timeout < 0:
-        LOG.error("Could not set server certificate '{0}' within 60 seconds on ELB '{1}:{2}' in region '{3}'.".format(server_certificate_arn, domain['elb'], domain['elb_port'], domain['elb_region']))
-        return False
-
-    LOG.debug("Set server certificate '{0}' on ELB '{1}:{2}' in region '{3}' in {4} seconds.".format(
-        server_certificate_arn,
-        domain['elb'],
-        domain['elb_port'],
-        domain['elb_region'],
-        60-timeout))
-
-    return True
-
-def answer_dns_challenge(conf, client, domain, challenge):
-    """
-    Compute the required answer and set it in the DNS record
-    for the domain.
-    """
-    authorization = "{}.{}".format(
-        base64.urlsafe_b64encode(challenge.get("token")).decode("ascii").replace("=", ""),
-        base64.urlsafe_b64encode(client.key.thumbprint()).decode("ascii").replace("=", "")
-        )
-
-    dns_response = base64.urlsafe_b64encode(hashlib.sha256(authorization.encode()).digest()).decode("ascii").replace("=", "")
-
-    # Let's update the DNS on our R53 account
-    zone_id = get_route53_zone_id(conf, domain['r53_zone'])
-    if zone_id == None:
-        LOG.error("Cannot determine zone id for zone '{0}'".format(domain['r53_zone']))
-        return None
-
-    LOG.info("Domain '{0}' has '{1}' for Id".format(domain['r53_zone'], zone_id))
-
-    zone_id = get_route53_zone_id(conf, domain['r53_zone'])
-    if zone_id == None:
-        LOG.error("Cannot find R53 zone {}, are you controling it ?".format(domain['r53_zone']))
-        return None
-
-    acme_domain = "_acme-challenge.{}".format(domain['name'])
-
-    res = reset_route53_letsencrypt_record(conf, zone_id, domain['name'], acme_domain)
-    if res == None:
-        LOG.error("An error occured while trying to remove a previous resource record. Skipping domain {0}".format(domain['name']))
-        return None
-
-    add_status = create_route53_letsencrypt_record(conf, zone_id, domain['name'], acme_domain, 'TXT', '"' + dns_response + '"')
-    if add_status == None:
-        LOG.error("An error occured while creating the dns record. Skipping domain {0}".format(domain['name']))
-        return None
-
-    add_status = wait_letsencrypt_record_insync(conf, add_status)
-    if add_status == None:
-        LOG.error("Cannot determine if the dns record has been correctly created. Skipping domain {0}".format(domain['name']))
-        return None
-
-    if add_status == False:
-        LOG.error("We updated R53 but the servers didn't sync within 60 seconds. Skipping domain {0}".format(domain['name']))
-        return None
-
-    if add_status is not True:
-        LOG.error("An unexpected result code has been returned. Please report this bug. Skipping domain {0}".format(domain['name']))
-        LOG.error("add_status={0}".format(add_status))
-        return None
-
-    ## Now, let's tell the ACME server that we are ready
-    challenge_response = challenges.DNS01Response(key_authorization=authorization)
-    challenge_resource = client.answer_challenge(challenge, challenge_response)
-
-    if challenge_resource.body.error != None:
-        return False
-
-    return True
-
-def create_and_save_key(conf, s3_key, kms_key='AES256'):
-    """
-    Generate a RSA 4096 key for general purpose (account or CSR)
-    """
-    LOG.info("Generating new RSA key")
-    key = RSA.generate(4096).exportKey("PEM")
-    save_to_s3(conf, s3_key, key, True, kms_key)
-    return key
+        save_to_s3(conf, domain['name']+".certificate.cert", certificate)
+        save_to_s3(conf, domain['name']+".certificate.key", certificate_key)
+        save_to_s3(conf, "account.key.rsa", account_key)
 
 def save_to_s3(conf, s3_key, content, encrypt=False, kms_key='AES256'):
     """
@@ -420,60 +225,11 @@ def save_to_s3(conf, s3_key, content, encrypt=False, kms_key='AES256'):
             kwargs['ServerSideEncryption'] = 'AES256'
 
     try:
-        s3.put_object(**kwargs);
+        s3.put_object(**kwargs)
     except ClientError as e:
         LOG.error("Failed to save '{0}' in bucket '{1}'".format(s3_key, conf['s3_bucket']))
         LOG.error("Error: {0}".format(e))
         return None
-
-def load_private_key(conf, domain):
-    key = None
-    name = domain['name'] + ".key.rsa"
-
-    if 'reuse_key' in domain.keys() and domain['reuse_key'] == True:
-        LOG.debug("Attempting to load private key from S3 for domain '{0}'".format(domain['name']))
-        key = load_from_s3(conf, name)
-
-    if key == None:
-        key = create_and_save_key(conf, name, domain['kmsKeyArn'])
-
-    return crypto.load_privatekey(crypto.FILETYPE_PEM, key)
-
-def generate_certificate_signing_request(conf, domain):
-    key = load_private_key(conf, domain)
-
-    LOG.info("Creating Certificate Signing Request.")
-    csr = crypto.X509Req()
-    csr.get_subject().countryName = domain['countryName']
-    csr.get_subject().CN = domain['name']
-    csr.set_pubkey(key)
-    csr.sign(key, "sha1")
-    return (csr, key)
-
-def request_certificate(conf, domain, client, auth_resource):
-    (csr, key) = generate_certificate_signing_request(conf, domain)
-
-    try:
-        (certificate, ar) = client.poll_and_request_issuance(ComparableX509(csr), [auth_resource])
-    except errors.PollError as e:
-        LOG.error("Failed to get certificate issuance for '{0}'.".format(domain['name']))
-        LOG.error("Error: {0}".format(e))
-        return (False, False, False)
-
-    chain = requests.get(certificate.cert_chain_uri)
-    chain_certificate = None
-
-    if chain.status_code == 200:
-        chain_certificate = crypto.load_certificate(crypto.FILETYPE_ASN1, chain.content)
-        pem_chain_certificate = crypto.dump_certificate(crypto.FILETYPE_PEM, chain_certificate).decode("ascii")
-    else:
-        LOG.error("Failed to retrieve chain certificate. Status was '{0}'.".format(chain.status_code))
-        pem_chain_certificate = False
-
-    pem_certificate = crypto.dump_certificate(crypto.FILETYPE_PEM, certificate.body.wrapped).decode("ascii")
-    pem_private_key = crypto.dump_privatekey(crypto.FILETYPE_PEM, key).decode("ascii")
-
-    return (pem_chain_certificate, pem_certificate, pem_private_key)
 
 def lambda_handler(event, context):
     if 'bucket' not in event:
@@ -509,7 +265,7 @@ def lambda_handler(event, context):
     s3_client = boto3.client('s3', config=Config(signature_version='s3v4', region_name=s3_region))
 
     conf = load_config(s3_client, s3_bucket, letslambda_config)
-    if conf == None:
+    if conf == None:    
         LOG.critical("Cannot load letslambda configuration. Exiting.")
         exit(1)
 
@@ -518,43 +274,38 @@ def lambda_handler(event, context):
     conf['s3_bucket'] = s3_bucket
     conf['letslambda_config'] = letslambda_config
     conf['kms_key'] = kms_key
+    request_certificate(conf)
 
-    account_key = load_letsencrypt_account_key(conf)
+def is_new(conf):
+    try:
+        load_from_s3(conf, 'account.key.rsa')
+    except:
+        return False
 
-    acme_client = client.Client(conf['directory'], account_key)
-    for domain in conf['domains']:
-        if 'r53_zone' not in domain.keys():
-            LOG.error("Missing parameter 'r53_zone' for domain '{0}'. Skipping domain.".format(domain['name']))
-            continue
+def request_certificate(conf):
+    dns_class = sewer.Route53()
+    # https://github.com/komuw/sewer/blob/43c3c8efae36489939d93096579ec54e941f67c7/sewer/client.py
+    # 1. to create a new certificate:
+    client = sewer.Client(domain_name=conf['domain_name'], 
+                        domain_alt_names=conf['domain_alt_names'], 
+                        contact_email=conf['contact_email'],
+                        dns_class=dns_class)
+    if is_new(conf):
+        certificate = client.cert()
+    else:
+        certificate = client.renew()
+    
+    # will need to switch apache to not use chain or extract it per this issue
+    certificate_key = client.certificate_key
+    #https://github.com/komuw/sewer/issues/97 to get chain
+    # openssl x509 -in some_certificate_and_chain.crt -text -noout
+    #chain = client.certificate_key
+    account_key = client.account_key
+    domain = conf['domain']
+    save_certificates_to_s3(conf, domain, certificate, certificate_key, account_key)
+    print("your certificate is:", certificate)
+    print("your certificate's key is:", certificate_key)
+    print("your letsencrypt.org account key is:", account_key)
+    # NB: your certificate_key and account_key should be SECRET.
+    # keep them very safe.
 
-        if 'kmsKeyArn' not in domain.keys():
-            domain['kmsKeyArn'] = conf['kms_key']
-
-        if 'reuse_key' not in domain.keys():
-            domain['reuse_key'] = True
-
-        if 'elb_port' not in domain.keys():
-            domain['elb_port'] = 443
-
-        if 'elb_region' not in domain.keys():
-            domain['elb_region'] = conf['region']
-
-        authorization_resource = get_authorization(acme_client, domain)
-        challenge = get_dns_challenge(authorization_resource)
-        res = answer_dns_challenge(conf, acme_client, domain, challenge)
-        if res is not True:
-            LOG.error("An error occurred while answering the DNS challenge. Skipping domain '{0}'.".format(domain['name']))
-            continue
-
-        (chain, certificate, key) = request_certificate(conf, domain, acme_client, authorization_resource)
-        if key == False or certificate == False:
-            LOG.error("An error occurred while requesting the signed certificate. Skipping domain '{0}'.".format(domain['name']))
-            continue
-
-        save_certificates_to_s3(conf, domain, chain, certificate)
-        iam_cert = upload_to_iam(conf, domain, chain, certificate, key)
-        if iam_cert is not False and iam_cert['ResponseMetadata']['HTTPStatusCode'] is 200 and 'elb' in domain.keys():
-            update_elb_server_certificate(conf, domain, iam_cert['ServerCertificateMetadata']['Arn'])
-        else:
-            LOG.error("An error occurred while saving your server certificate in IAM. Skipping domain '{0}'.".format(domain['name']))
-            continue
